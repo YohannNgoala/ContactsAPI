2aaf0f263178b725e064a0493ad7c37407b742f6
yohan <pcTek>
2020-09-02 22:27:45 +0200

    Cpp->Cp

---
+++ Models/ContactsContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ContactsApi.Models
+{
+    public class ContactsContext : DbContext
+    {
+        public ContactsContext(DbContextOptions<ContactsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ContactsItem> ContactsItems { get; set; }
+    }
+}
---
+++ Models/ContactsItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class ContactsItem
+{
+    public long Id { get; set; }
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+    [Required]
+    public string FullName { get; set; }
+    [Required]
+    public string Address { get; set; }
+    [Required]
+    public string Email { get; set; }
+    [Required]
+    public string Phone { get; set; }
+    public SkillsItem Skill{ get; set; }   
+}
